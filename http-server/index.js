35d74ab1654e4421fb524e0d16d40d177f71d9b9
Don-Alvin <alvindon41@gmail.com>
2023-01-26 13:00:43 +0300

    Request and Response and streams

--- http-server/index.js
+++ http-server/index.js
@@ -1,11 +1,56 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-	res.writeHead(200, {
-		"Content-Type": "text/plain",
-	});
+const server = http.createServer();
 
-	res.end("Hello! Sir Isaac Newton is your Friend");
+const friends = [
+	{
+		id: 0,
+		name: "Abraham Enstein",
+	},
+
+	{
+		id: 1,
+		name: "Isaac Newton",
+	},
+];
+
+server.on("request", (req, res) => {
+	const items = req.url.split("/");
+
+	if (req.method === "POST" && items[1] === "friends") {
+		req.on("data", (data) => {
+			const friend = data.toString();
+			console.log("Request:", data);
+			friends.push(JSON.parse(friend));
+			req.pipe(res);
+		});
+	} else if (req.method === "GET" && items[1] === "friends") {
+		res.statusCode = 200;
+		res.setHeader("Content-Type", "application/json");
+		if (items.length === 3) {
+			const friendIndex = Number(items[2]);
+			res.end(JSON.stringify(friends[friendIndex]));
+		} else {
+			res.end(JSON.stringify(friends));
+		}
+	} else if (req.method === "GET" && items[1] === "messages") {
+		res.write(
+			`<html>
+                <body>
+                    <ul>
+                        <li>Hello Isaac!</li>
+                        <li>What are your thoughts on Astronomy</li>
+                    </ul>
+                </body>
+             </html>`
+		);
+		res.end();
+	} else {
+		res.statusCode = 400;
+		res.end();
+	}
+
+	console.log(items);
 });
 
 server.listen(5000, () => {
